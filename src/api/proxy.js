19eb6f5970e92fa80e0b878436e1115024fea1a0
saas <joho.sz@foxmail.com>
2023-05-08 21:26:26 +0800

    完成登陆

--- src/api/proxy.js
+++ src/api/proxy.js
@@ -2,7 +2,7 @@
 const {createProxyMiddleware} = require('http-proxy-middleware')
 
 module.exports = (req, res) => {
-    let target = ''
+    let target = 'http://ceshi13.dishait.cn/admin'
 
     // 代理目标地址
     // xxxxx 替换为你跨域请求的服务器 如： http://baidu.com
@@ -17,7 +17,7 @@ module.exports = (req, res) => {
         pathRewrite: {
             // 通过路径重写，去除请求路径中的 `/api`
             // 例如 /api/user/login 将被转发到 http://target/user/login
-            '^/api/': 'http://ceshi13.dishait.cn/admin'
+            '^/api/': '/'
         }
     })(req, res)
 }
