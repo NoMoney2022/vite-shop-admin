2803265952507943269f77cdfb5a1a3833e834cd
saas <joho.sz@foxmail.com>
2023-05-08 22:39:12 +0800

    完成登陆

--- src/api/proxy.js
+++ src/api/proxy.js
@@ -1,24 +1,32 @@
 // 该服务为 vercel serve跨域处理
-const {createProxyMiddleware} = require('http-proxy-middleware')
+// const {createProxyMiddleware} = require('http-proxy-middleware')
 
-module.exports = (req, res) => {
-    let target = 'http://ceshi13.dishait.cn/admin'
+export default function proxy(req, res) {
 
-    // 代理目标地址
-    // xxxxx 替换为你跨域请求的服务器 如： http://baidu.com
-    if (req.url.startsWith('/api')) {
-        // 这里填目标地址
-        target = 'xxxxx'
-    }
-    // 创建代理对象并转发请求
-    createProxyMiddleware({
-        target,
-        changeOrigin: true,
-        pathRewrite: {
-            // 通过路径重写，去除请求路径中的 `/api`
-            // 例如 /api/user/login 将被转发到 http://target/user/login
-            '^/api/': '/'
-        }
-    })(req, res)
+    res.status(200).json({
+        body: req.body,
+        query: req.query,
+        cookies: req.cookies,
+    });
+
+
+    // let target = 'http://ceshi13.dishait.cn/admin'
+    //
+    // // 代理目标地址
+    // // xxxxx 替换为你跨域请求的服务器 如： http://baidu.com
+    // if (req.url.startsWith('/api')) {
+    //     // 这里填目标地址
+    //     target = 'xxxxx'
+    // }
+    // // 创建代理对象并转发请求
+    // createProxyMiddleware({
+    //     target,
+    //     changeOrigin: true,
+    //     pathRewrite: {
+    //         // 通过路径重写，去除请求路径中的 `/api`
+    //         // 例如 /api/user/login 将被转发到 http://target/user/login
+    //         '^/api/': '/'
+    //     }
+    // })(req, res)
 }
 
